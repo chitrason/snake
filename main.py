d6d7befdba58fdeba4eba16b419d2647826965f9
Mcswiggi <mrchitrason@gmail.com>
2022-11-15 20:30:17 -0500

    implemented move snake

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ import time
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
-screen.title('Snake')
+screen.title('Snake') 
 screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
 game_is_on = True
 # moves snake
@@ -20,5 +25,5 @@ while game_is_on:
 
   snake.move()
 
-
 screen.exitonclick()
+
