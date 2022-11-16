e2a4acf8b81e10d0c0a746d17731eaba736a307f
Mcswiggi <mrchitrason@gmail.com>
2022-11-15 21:12:06 -0500

    add game over when hitting wall

--- scoreboard.py
+++ scoreboard.py
@@ -12,7 +12,11 @@ class Scoreboard(Turtle):
     self.update_scoreboard()
   
   def update_scoreboard(self):
-    self.write(f'Score: {self.score}', align='center', font=('Arial', 24, 'normal'))
+    self.write(f'Score: {self.score}', align='center', font=('Courier', 24, 'normal'))
+
+  def game_over(self):
+    self.goto(0,0)
+    self.write(f'GAME OVER', align='center', font=('Courier', 24, 'normal'))
 
 
   def increase_score(self):
